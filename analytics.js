054e4b357a9d3a22e85c7f22e4d8988b699a58fc
Stas0889 <stas0889@local>
2026-04-28 15:37:03 +0300

    Improve SEO metadata and analytics setup

---
+++ analytics.js
@@ -0,0 +1,39 @@
+(function () {
+  var YANDEX_METRIKA_ID = "";
+  var GOOGLE_ANALYTICS_ID = "";
+
+  if (YANDEX_METRIKA_ID) {
+    (function (m, e, t, r, i, k, a) {
+      m[i] = m[i] || function () {
+        (m[i].a = m[i].a || []).push(arguments);
+      };
+      m[i].l = 1 * new Date();
+      k = e.createElement(t);
+      a = e.getElementsByTagName(t)[0];
+      k.async = 1;
+      k.src = r;
+      a.parentNode.insertBefore(k, a);
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    window.ym(YANDEX_METRIKA_ID, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    });
+  }
+
+  if (GOOGLE_ANALYTICS_ID) {
+    var gtagScript = document.createElement("script");
+    gtagScript.async = true;
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(GOOGLE_ANALYTICS_ID);
+    document.head.appendChild(gtagScript);
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function () {
+      window.dataLayer.push(arguments);
+    };
+    window.gtag("js", new Date());
+    window.gtag("config", GOOGLE_ANALYTICS_ID);
+  }
+})();
